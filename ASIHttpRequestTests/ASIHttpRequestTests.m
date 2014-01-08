3b7d8a2a3a8b8ba6c78f349888934da3c38f4058
david.liu <liuchong1228@yahoo.cn>
2014-01-08 12:28:44 +0800

    Initial Commit

---
+++ ASIHttpRequestTests/ASIHttpRequestTests.m
@@ -0,0 +1,34 @@
+//
+//  ASIHttpRequestTests.m
+//  ASIHttpRequestTests
+//
+//  Created by david on 14-1-8.
+//  Copyright (c) 2014年 david. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ASIHttpRequestTests : XCTestCase
+
+@end
+
+@implementation ASIHttpRequestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
